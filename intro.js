46bc5dc945e4ecb88a872ff9448b7278220068f6
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-03-21 09:36:38 +0100

    main update

---
+++ intro.js
@@ -0,0 +1,4 @@
+// alert("Ciao!"); // Finestra pop-up con scritta
+// confirm("Ok?"); // Crea finestra di conferma. Ok = true, Annulla = false
+// prompt("Dammi il tuo nome"); //Finestra di inserimento
+// let myVariable = prompt("dammi numero"); // assegna alla let il valore inserito
