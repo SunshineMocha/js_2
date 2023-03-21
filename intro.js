6ac3f569081e839db8d91d229aed12357725ef16
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-03-21 10:51:06 +0100

    update main

--- intro.js
+++ intro.js
@@ -2,3 +2,19 @@
 // confirm("Ok?"); // Crea finestra di conferma. Ok = true, Annulla = false
 // prompt("Dammi il tuo nome"); //Finestra di inserimento
 // let myVariable = prompt("dammi numero"); // assegna alla let il valore inserito
+// alert(prompt("Digita nome")); chiede nome e poi mostra
+// let myVar = prompt(" Digita il tuo nome" +  myVar, "John"); // mostra un prompt che assegna a myvar, con un valore esempio definito nella casella testo
+// isNaN("a") = true isNaN(5) = false // is NOT a number 
+
+// Il seguente codice confronta se quello che è stato inserito dall'utente è un numero o una stringa
+
+/*
+    let digita = prompt("Digita qualcosa");
+    let result = isNaN(digita);
+    if (result === true){
+        alert("Non è un numero");
+    }
+    else{
+        alert("È un numero");
+    }
+*/
